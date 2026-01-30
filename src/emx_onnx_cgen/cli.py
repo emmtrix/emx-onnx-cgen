eca015f54b89aa90920379cc5bd2c0664d05614d
Timo Stripf <timo.stripf@emmtrix.com>
2026-01-30 23:08:51 +0100

    docs: document accumulation flags (#416)

--- src/emx_onnx_cgen/cli.py
+++ src/emx_onnx_cgen/cli.py
@@ -353,6 +353,19 @@ def _build_parser() -> argparse.ArgumentParser:
             ),
         )
 
+    def add_fp16_accumulation_strategy_flag(
+        subparser: argparse.ArgumentParser,
+    ) -> None:
+        subparser.add_argument(
+            "--fp16-accumulation-strategy",
+            choices=("simple", "fp32"),
+            default="fp32",
+            help=(
+                "Accumulation strategy for float16 inputs "
+                "(simple uses float16, fp32 uses float; default: fp32)"
+            ),
+        )
+
     compile_parser = subparsers.add_parser(
         "compile", help="Compile an ONNX model into C source"
     )
@@ -428,6 +441,7 @@ def _build_parser() -> argparse.ArgumentParser:
     )
     add_restrict_flags(compile_parser)
     add_fp32_accumulation_strategy_flag(compile_parser)
+    add_fp16_accumulation_strategy_flag(compile_parser)
 
     verify_parser = subparsers.add_parser(
         "verify",
@@ -553,6 +567,7 @@ def _build_parser() -> argparse.ArgumentParser:
     )
     add_restrict_flags(verify_parser)
     add_fp32_accumulation_strategy_flag(verify_parser)
+    add_fp16_accumulation_strategy_flag(verify_parser)
     return parser
 
 
@@ -665,6 +680,7 @@ def _compile_model(
             model_checksum=model_checksum,
             restrict_arrays=args.restrict_arrays,
             fp32_accumulation_strategy=args.fp32_accumulation_strategy,
+            fp16_accumulation_strategy=args.fp16_accumulation_strategy,
             truncate_weights_after=args.truncate_weights_after,
             large_temp_threshold_bytes=args.large_temp_threshold_bytes,
             large_weight_threshold=args.large_weight_threshold,
@@ -872,6 +888,7 @@ def _verify_model(
             model_checksum=model_checksum,
             restrict_arrays=args.restrict_arrays,
             fp32_accumulation_strategy=args.fp32_accumulation_strategy,
+            fp16_accumulation_strategy=args.fp16_accumulation_strategy,
             truncate_weights_after=args.truncate_weights_after,
             large_temp_threshold_bytes=args.large_temp_threshold_bytes,
             large_weight_threshold=args.large_weight_threshold,
